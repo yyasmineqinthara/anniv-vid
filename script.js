8704609128d530c12052910c5e018d4835811450
yyasmineqinthara <maharaniqinthara@gmail.com>
2024-11-02 08:12:15 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Menambahkan pesan ketika video selesai diputar
+document.querySelector("video").addEventListener("ended", function() {
+    alert("Thank you for watching! Happy Anniversary!");
+});
